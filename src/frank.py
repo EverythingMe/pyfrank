295a36c9ee91b6e7bcd2f756b8b22d1981a8b220
Daniel Ben-Zvi <daniel.benzvi@gmail.com>
2012-10-04 17:48:22 +0200

    Added licensing information

--- src/frank.py
+++ src/frank.py
@@ -23,6 +23,7 @@
 # The views and conclusions contained in the software and documentation are those of the
 # authors and should not be interpreted as representing official policies, either expressed
 # or implied, of Do@.
+
 __author__ = 'Daniel Ben-Zvi'
 
 import sys
